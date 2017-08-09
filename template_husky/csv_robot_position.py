b7cbfcd694aee4204c1e0ef937116052be14b61f
Mahmoud Akl <akl@epfl.ch>
2017-08-09 16:38:50 +0200

    [NRRPLT-5454] uploading tf csv_robot_position gives error
    Change-Id: I3971ab03507c6f87ef167fe842bf3ab00918abc0

--- template_husky/csv_robot_position.py
+++ template_husky/csv_robot_position.py
@@ -2,6 +2,7 @@
 @nrp.MapRobotSubscriber("position", Topic('/gazebo/model_states', gazebo_msgs.msg.ModelStates))
 @nrp.Robot2Neuron()
 def csv_robot_position(t, position, recorder):
-    recorder.record_entry(position.value.pose[-1].position.x,
-                          position.value.pose[-1].position.y,
-                          position.value.pose[-1].position.z)
+    if not isinstance(position.value, type(None)):
+        recorder.record_entry(position.value.pose[-1].position.x,
+                              position.value.pose[-1].position.y,
+                              position.value.pose[-1].position.z)
