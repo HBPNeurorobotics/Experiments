1d82a0e680af79f03d8ab56f7910459ffa48e7cb
Mahmoud Akl <mahmoud.akl@tum.de>
2017-09-10 15:28:34 +0200

    [NRRPLT-5596] csv_robot_position TF has the robot pose index hardcoded
    This patch removes the hardocded robot index in the list of models
    and searches for it at t = 0. This needs to be done because the order
    of the models changes upon reset.
    
    Change-Id: Ie857a5369ca029a1bb93e9171ca8d13de4575645

--- template_pioneer/csv_robot_position.py
+++ template_pioneer/csv_robot_position.py
@@ -1,7 +1,13 @@
 @nrp.MapCSVRecorder("recorder", filename="robot_position.csv", headers=["x", "y", "z"])
 @nrp.MapRobotSubscriber("position", Topic('/gazebo/model_states', gazebo_msgs.msg.ModelStates))
+@nrp.MapVariable("robot_index", global_key="robot_index", initial_value=None)
 @nrp.Robot2Neuron()
-def csv_robot_position(t, position, recorder):
-    recorder.record_entry(position.value.pose[-1].position.x,
-                          position.value.pose[-1].position.y,
-                          position.value.pose[-1].position.z)
+def csv_robot_position(t, position, recorder, robot_index):
+    if t == 0:
+        robot_index.value = None
+    if not isinstance(position.value, type(None)):
+        if robot_index.value is None:
+            robot_index.value = position.value.name.index('robot')
+        recorder.record_entry(position.value.pose[robot_index.value].position.x,
+                              position.value.pose[robot_index.value].position.y,
+                              position.value.pose[robot_index.value].position.z)
