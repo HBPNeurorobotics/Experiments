fad8ca6044b812468d2a26c2d56b68dd0308c642
Manos Angelidis <angelidis@fortiss.org>
2018-12-12 10:21:10 +0000

    Merged in NRRPLT-6660 (pull request #11)
    [NRRPLT-6660] Remove all CSV recorders from experiments and create a dedicated csv recording experiment
    
    * [NNRPLT-6660] Remove all CSV recorders from experiments and create a dedicated csv recording experiment
    
    * [NNRPLT-6660] Remove all CSV recorders from experiments and create a dedicated csv recording experiment
    
    Approved-by: Luc Guyot <luc.guyot.ge@gmail.com>

--- tigrillo-cl-snn-learning/csv_spike_monitor.py
+++
@@ -1,7 +0,0 @@
-# Imported Python Transfer Function
-import hbp_nrp_cle.tf_framework as nrp
-@nrp.NeuronMonitor(nrp.brain.sensor_population0, nrp.spike_recorder)
-def csv_spike_monitor(t):
-        # Uncomment to log into the 'log-console' visible in the simulation
-        # clientLogger.info("Time: "+str(t))
-        return True
